42db6408a4f4085406f6a09f0453071898643a97
samesense <samesense@35a78be9-df47-0410-acef-f5dffbb544d5>
2008-03-24 22:36:54 +0000

    upload kegg code

---
+++ trunk/perry/Get_Data/getHumanKEGG.rb
@@ -0,0 +1,17 @@
+require 'bio'
+
+serv = Bio::KEGG::API.new
+pathways = serv.list_pathways('hsa')
+pathways.each do |path|
+	      genes = serv.get_genes_by_pathway(path.entry_id)
+	      genes.each do |gene|
+	      		 puts gene + ' ' + path.entry_id
+	      end
+end
+
+#genes = serv.get_genes_by_organism('hsa', 0, 40000)
+## ls = serv.get_genes_by_pathway('path:eco00020')
+#genes.each do |gene|
+#	   paths = serv.get_pathways_by_genes([gene])
+#	   puts gene, paths.length
+#end
