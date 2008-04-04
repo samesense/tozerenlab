39e4c8540d22b681e8ec65f426a5c401517837a4
samesense <samesense@35a78be9-df47-0410-acef-f5dffbb544d5>
2008-04-04 18:24:26 +0000

    add+webgestalt+parser

---
+++ perry/Parse_Data/parseWebGestalt_details.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import sys
+
+if len(sys.argv) == < 3:
+    print 'enter the fileName & the order you want the fields in'
+    sys.exit(0)
+
+def removeQuotes(s):
+    return s[1:-2].strip()
+
+order = []
+for o in sys.argv[2:]:
+    order.append( int(o) )
+
+f = open(sys.argv[1])
+f.readline()
+for line in f.xreadlines():
+    sp = map(removeQuotes, line.split('\t'))
+    line = ''
+    for o in order:
+        line = line + sp[o] + '\t'
+    print line.strip()
+f.close()
