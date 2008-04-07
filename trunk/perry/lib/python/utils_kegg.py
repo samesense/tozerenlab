e0284ae873853ce68c1eeb8e1c4762e80b73117f
samesense <samesense@35a78be9-df47-0410-acef-f5dffbb544d5>
2008-04-07 22:57:59 +0000

    kegg+ko+functions+for+pathway+pics

--- trunk/perry/lib/python/utils_kegg.py
+++ trunk/perry/lib/python/utils_kegg.py
@@ -1,6 +1,25 @@
 from SOAPpy import WSDL
 
+def getGene2KO(path):
+    wsdl = 'http://soap.genome.jp/KEGG.wsdl'
+    serv = WSDL.Proxy(wsdl)
+    kos = serv.get_kos_by_pathway(path)
+    gene2ko = dict()
+    for ko in kos:
+        genes = serv.get_genes_by_ko(ko)
+        for gene in genes:
+            gene = gene['entry_id']
+            if not gene2ko.has_key(gene):
+                gene2ko[gene] = dict()
+            gene2ko[gene][ko] = True
+    return gene2ko
 
+def junk(path):
+   wsdl = 'http://soap.genome.jp/KEGG.wsdl'
+   serv = WSDL.Proxy(wsdl)
+   elements = serv.get_elements_by_pathway('path:hsa04660')
+   print elements
+   
 def printEdges(species_code):
     wsdl = 'http://soap.genome.jp/KEGG.wsdl'
     serv = WSDL.Proxy(wsdl)
@@ -98,7 +117,7 @@ def getPathwayGenes(path):
                 genes[n.split(':')[1]] = True
     return genes
 
-def getPathwayComplexDict(path):
+def getKODictForPathway(path):
     complex2gene = dict()
     gene2complex = dict()
     wsdl = 'http://soap.genome.jp/KEGG.wsdl'
@@ -115,4 +134,50 @@ def getPathwayComplexDict(path):
                 if not gene2complex.has_key(n): gene2complex[n] = dict()
                 gene2complex[n][elementID] = True
     return [complex2gene, gene2complex]
+
+def getGraphicLinks(path):
+    gene2ko = getGene2KO(path)
+    edges = dict()
+    wsdl = 'http://soap.genome.jp/KEGG.wsdl'
+    serv = WSDL.Proxy(wsdl)
+    elements = serv.get_elements_by_pathway(path)
+    relations = serv.get_element_relations_by_pathway(path)
+    elementid2gene = dict()
+    for element in elements:
+        if element['type'] == 'gene':
+            elementid = element['element_id']
+            names = element['names']
+            elementid2gene[elementid] = dict()
+            for n in names:
+                elementid2gene[elementid][n] = True
+
+    for relation in relations:
+        k1 = relation['element_id1']
+        k2 = relation['element_id2']
+        if elementid2gene.has_key(k1) and elementid2gene.has_key(k2): 
+            elements1 = elementid2gene[ k1 ]
+            elements2 = elementid2gene[ k2 ]
+            for gene1 in elements1.keys():
+                for gene2 in elements2.keys():
+                    for s in relation['subtypes']:
+                        ko1 = ''
+                        ko2 = ''
+                        if gene2ko.has_key(gene1):
+                            ko1 = gene2ko[gene1]
+                        if gene2ko.has_key(gene2):
+                            ko2 = gene2ko[gene2]
+                        if ko1 != '' and ko2 != '':
+                            print ko1, ko2
+                        else: print 'missing'
+                        #if not edges.has_key(gene1):
+                        #    edges[gene1] = dict()
+                        #if not edges.has_key(gene2):
+                        #    edges[gene2] = dict()
+                        #if not edges[gene1].has_key(gene2):
+                        #    edges[gene1][gene2] = dict()
+                        #if not edges[gene2].has_key(gene1):
+                        #    edges[gene2][gene1] = dict()
+                        #edges[gene1][gene2][ relation['type'] + '/' + s['relation'] ] = True
+                        #edges[gene2][gene1][ relation['type'] + '/' + s['relation'] ] = True
+    return edges
 
