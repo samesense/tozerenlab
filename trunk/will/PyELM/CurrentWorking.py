a3e342a606aa9a7ec34fc812ee40162e102b2834
walldo2 <walldo2@35a78be9-df47-0410-acef-f5dffbb544d5>
2008-04-23 18:35:01 +0000

    Fixed iterators again ... added test_data.fasta for easy prototyping and error checking

--- trunk/will/PyELM/CurrentWorking.py
+++ trunk/will/PyELM/CurrentWorking.py
@@ -1,12 +1,33 @@
 import os
-import Bio
-import numpy
+from Bio import SeqIO
+from numpy import *
 import re
+from pylab import *
 
 os.chdir(os.curdir)
 
-import ELMFunctions
+from PyELM import *
 
 
+bkgHandle = open('test_data.fasta','rU')
+bkgSeqs=[]
+
+for thisSeq in SeqIO.parse(bkgHandle,'fasta'):
+    bkgSeqs.append(thisSeq.seq.tostring())
+
+bkgHandle.close()
+
+ELMAnal = PyELM()
+
+ELMAnal.ELMParser()
+ELMAnal.LoadSeqs(bkgSeqs)
+
+ELMAnal.ELMMatchSeqs()
+
+counter=-1
+for thisSeq in ELMAnal.GetSeqIterator():
+    counter += 1
+    print counter
+    print sum(thisSeq)
 
 
--- trunk/will/PyELM/PyELM.py
+++ trunk/will/PyELM/PyELM.py
@@ -7,8 +7,6 @@ print 'Actually running'
 
 class PyELM:
 
-
-
     def __init__(self):
 
         self.ELMDict={}
@@ -20,7 +18,6 @@ class PyELM:
 
         self.PreCalchistArray=[]        
         self.PreCalcELMMatchSeqs=[]
-        self.PreCalcELMPosDict={}
 
     def ELMParser(self,DIRECTORY="C:\Documents and Settings\Will\My Documents\ELM_Motif_finder\ELM_RAW_DOWNLOAD\\"):
         """
@@ -159,13 +156,13 @@ class PyELM:
         
         return self.PreCalchistArray
 
-    def ELMPosGen(self,FORCE_CALC=False):
+    def ELMPosGen(self,SEQ_IND,POS_START,POS_STOP,WANTED_ELM):
         """
         ELMPosGen
             Generates a [len(ELM_MATCH_VEC) MAX_SIZE] matrix in which each position is a 1 if the ELM is present at that location
         and 0 otherwise.
 
-            ELM_POS_DICT = ELMPosGen()
+            ELM_POS_DICT = ELMPosGen(POS_START,POS_STOP,WANTED_ELM)
 
         ELM_POS_DICT    A DICT which is 'keyed' by ELM name and each value is a matrix.
 
@@ -174,28 +171,28 @@ class PyELM:
         Will return only the desired array.
 
         """
-        if len(self.ELMDict) == 0:
-            print 'ELMParser must be run first!'
-            return
-        if len(self.Sequences) == 0:
-            print 'LoadSeqs must be run first!'
-            return
+
+        #check to see if calculation can be performed
         if len(self.PreCalcELMMatchSeqs) == 0:
             self.ELMMatchSeqs()
-        else:
-            print 'Have ElmMatchSeqs'
 
-        if len(self.PreCalcELMPosDict) == 0 | FORCE_CALC:
-            ELM_POS_DICT={}
-            for thisELM in self.ELMOrder:
-                boolArray = zeros((len(self.PreCalcELMMatchSeqs),self.MaxSize))
-                for i in xrange(len(self.PreCalcELMMatchSeqs)):
-                    boolArray[i,self.PreCalcELMMatchSeqs[i][thisELM]]=1
-                ELM_POS_DICT[thisELM] = boolArray
-            self.PreCalcELMPosDict=ELM_POS_DICT
+        if POS_START < 0 | POS_STOP <= POS_START | POS_START > self.MaxSize | POS_STOP > self.MaxSize:
+            print 'Arguements to POS_START >0 & POS_STOP > POS_START'
+            raise IndexError
 
+        if SEQ_IND > len(self.Sequences) | SEQ_IND < 0:
+            print 'Arguement SEQ_IND must be between 0 and len(self.Sequences)'
+            raise IndexError
 
-        return self.PreCalcELMPosDict
+        if not(WANTED_ELM in self.ELMOrder):
+            print 'WANTED_ELM not found in ELMOrder'
+            raise KeyError
+
+        boolArray = zeros((1,POS_STOP-POS_START),'bool')
+        for i in xrange(POS_START,POS_STOP):
+            boolArray[0,i-POS_START]=i in self.PreCalcELMMatchSeqs[SEQ_IND][WANTED_ELM]
+            
+        return boolArray
 
     def GetELMIterator(self):
         """
@@ -205,7 +202,7 @@ class PyELM:
         return self.ELMIterator(self)
         
 
-    class ELMIterator:
+    class ELMIterator():
         def __init__(self,instance):
             self.__list=instance.ELMOrder
             self.__thisSpot=-1
@@ -221,48 +218,48 @@ class PyELM:
             else:
                 raise StopIteration
     
-    def GetSeqIterator(self,TYPE,WANTED_ELM=None):
+    def GetSeqIterator(self,WANTED_ELM=None,WANTED_RANGE=None):
         """
         GetSeqIterator
             Will allow for easy iteration and retrival of data in a Seq-Order manner.
 
-        GetSeqIterator('ELMMatchSeqs',WANTED_ELM)
-            This creates an iterator which will return the ELMMatchSeqs data as an array in Sequence order.  If
-            WANTED_ELM is left empty then a DICT is returned at each iteration
-        GetSeqIterator('ELMPosDict',WANTED_ELM)
+        GetSeqIterator('ELMPosDict',WANTED_ELM,WANTED_RANGE)
+            WANTED_RANGE        A tuple indicating the range of positions desired.  If left empty then the
+                                entire sequence is provided.
+            
             This creates an iterator which will return the ELMPos Array data in Sequence order. If WANTED_ELM
             is left empty then Data is returned as an Array in which each row represents one ELM.
         """
         if (WANTED_ELM == None) | (WANTED_ELM in self.ELMOrder):
-            if TYPE == 'ELMMatchSeqs':
-                if len(self.PreCalcELMMatchSeqs) == 0:
-                    self.ELMMatchSeqs()
-            elif TYPE == 'ELMPosDict':
-                if len(self.PreCalcELMMatchSeqs) == 0:
-                    self.ELMPosGen()
-            else:
-                print 'A undefined Type was provided'
-                raise KeyError
-
-
-            return self.SeqIterator(self,TYPE,WANTED_ELM)
+            if WANTED_RANGE == None:
+                WANTED_RANGE = (0,self.MaxSize-1)
+            elif len(WANTED_RANGE) != 2:
+                print 'WANTED_RANGE must be len == 2 or None'
+                raise IndexError
+            elif WANTED_RANGE[0] < 0 | WANTED_RANGE[1] < WANTED_RANGE[0]:
+                print 'WANTED_RANGE[0] > 0 and WANTED_RANGE[1] > WANTED_RANGE[0]'
+                raise IndexError
+            elif WANTED_RANGE[0] > self.MaxSize | WANTED_RANGE[1] > self.MaxSize:
+                print 'WANTED_RANGE[0] < MaxSize and WANTED_RANGE[1] < MaxSize'
+                raise IndexError
+               
+            return self.SeqIterator(self,WANTED_ELM,WANTED_RANGE)
         else:
             print 'Provided an unknown ELM key'
             raise KeyError
             
 
     class SeqIterator:
-        def __init__(self,instance,TYPE,WANTED_ELM):
-            self.__MyType=TYPE
+        def __init__(self,instance,WANTED_ELM,WANTED_RANGE):
             self.__WantedELM=WANTED_ELM
+            self.__WantedRange=WANTED_RANGE
             self.__listSize=len(instance.Sequences)
             self.__thisSpot=-1
-            if TYPE == 'ELMMatchSeqs':
-                self.__ELMMatchSeqs=instance.PreCalcELMMatchSeqs
-            elif TYPE == 'ELMPosDict':
-                self.__MaxSize=instance.MaxSize
-                self.__ELMOrder=instance.ELMOrder
-                self.__ELMPosDict=instance.PreCalcELMPosDict
+            self.__MaxSize=instance.MaxSize
+            self.__ELMOrder=instance.ELMOrder
+            self.ELMOrder=instance.ELMOrder
+            self.PreCalcELMMatchSeqs=instance.PreCalcELMMatchSeqs
+            self.ELMPosGen=instance.ELMPosGen
 
         def __iter__(self):
             return self
@@ -270,19 +267,12 @@ class PyELM:
         def next(self):
             self.__thisSpot += 1
             if self.__thisSpot < self.__listSize:
-                if self.__MyType == 'ELMMatchSeqs':
-                    if self.__WantedELM == None:
-                        return self.__ELMMatchSeqs[self.__thisSpot]
-                    else:
-                        return self.__ELMMatchSeqs[self.__thisSpot][self.__WantedELM]
-                elif self.__MyType == 'ELMPosDict':
-                    if self.__WantedELM != None:
-                        return self.__ELMPosDict[self.__WantedELM][self.__thisSpot,:]
-                    else:
-                        OutArray=zeros((len(self.__ELMOrder),self.__MaxSize))
-                        for i in xrange(len(self.__ELMOrder)):
-                            OutArray[i,:]=self.__ELMPosDict[self.__ELMOrder[i]][self.__thisSpot]
-
-                        return OutArray
+                if self.__WantedELM != None:
+                    return self.ELMPosGen(self.__thisSpot,self.__WantedRange[0],self.__WantedRange[1],self.__WantedELM)
+                else:
+                    OutArray=zeros((len(self.__ELMOrder),self.__WantedRange[1]-self.__WantedRange[0]))
+                    for i in xrange(len(self.__ELMOrder)):
+                        OutArray[i,:]=self.ELMPosGen(self.__thisSpot,self.__WantedRange[0],self.__WantedRange[1],self.__ELMOrder[i])
+                    return OutArray
             else:
                 raise StopIteration
