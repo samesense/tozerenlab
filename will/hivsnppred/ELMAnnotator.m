38b4c04c096fd90c2dff471bd75cabc71ca92e8d
walldo2 <walldo2@35a78be9-df47-0410-acef-f5dffbb544d5>
2008-05-13 15:18:47 +0000

    finished changing ELMAnnotator.m for the new positional and PWM-inal data in a PosELMStruct as created by CreatePosAnnot.

--- will/hivsnppred/ELMAnnotator.m
+++ will/hivsnppred/ELMAnnotator.m
@@ -15,7 +15,7 @@ function PAT_STRUCT=ELMAnnotator(HIV_STRUCT,PAT_STRUCT,ELM_STRUCT)
 %       See also: ELMDownloadParser
 %
 
-WAITBAR_HANDLE=waitbar(0,'Processing Inputs');
+%WAITBAR_HANDLE=waitbar(0,'Processing Inputs');
 
 if ~isstruct(ELM_STRUCT)||~isfield(ELM_STRUCT,'REG_EXPR')
     error('ELMAnnotator:BAD_ELM_STRUCT','ELM_STRUCT must contain a REG_EXPR field as created by ELMDownloadParser.')
@@ -31,13 +31,13 @@ stop_indexes=find(strcmp('*',trans_table(:,2)));
 
 [ALIGNMENT_CELLS ALIGNMENT_INDS]=PatientStructHelper(PAT_STRUCT,{'Alignment_CELL','explodeCells'});
 
-waitbar(0,WAITBAR_HANDLE,'Translating')
+%waitbar(0,WAITBAR_HANDLE,'Translating')
 SEQS=cell(size(ALIGNMENT_CELLS));
 MAPPING=cell(size(ALIGNMENT_CELLS));
-tic
+%tic
 for i=1:length(ALIGNMENT_CELLS)
-    time=(toc/i)*(length(ALIGNMENT_CELLS)-i);
-    waitbar(i/length(ALIGNMENT_CELLS),WAITBAR_HANDLE, ['Translating: ' num2str(time/60)])
+    %time=(toc/i)*(length(ALIGNMENT_CELLS)-i);
+    %waitbar(i/length(ALIGNMENT_CELLS),WAITBAR_HANDLE, ['Translating: ' num2str(time/60)])
     temp=ALIGNMENT_CELLS{i};
     [SEQS{i} MAPPING{i}]=HIVORFChecker(HIV_STRUCT,temp(3,isletter(temp(3,:))));
     %    [SEQS{i} MAPPING{i}]=Translate(temp(3,:),1,nnz(isletter(temp(3,:)))/3);
@@ -57,13 +57,23 @@ ELMBinned = cell(length(SEQS),length(ELM_STRUCT));
 ELMPWMScore = cell(length(SEQS),length(ELM_STRUCT));
 ELMAnnot=cell(length(ELM_STRUCT),1);
 
-tic
+[ELMAnnot{:}]=deal(ELM_STRUCT(:).PosBins);
+for i=1:length(ELMAnnot)
+    if ~isempty(ELMAnnot{i})
+        ELMAnnot{i} = [i*ones(1,size(ELMAnnot{i},2)); ELMAnnot{i}];
+    end
+end
+
+AllBins = cat(2,ELMAnnot{:});
+
+%tic
 for i = 1:length(ELM_STRUCT)
-    time=(toc/i)*(length(length(ELM_STRUCT))-i);
-    waitbar(i/length(length(ELM_STRUCT)),WAITBAR_HANDLE, ['Applying Bins: ' num2str(time/60)])
+    %time=(toc/i)*(length(length(ELM_STRUCT))-i);
+    %waitbar(i/length(length(ELM_STRUCT)),WAITBAR_HANDLE, ['Applying Bins: ' num2str(time/60)])
     if ~isempty(ELM_STRUCT(i).PosBins)
         ELMBinned(:,i) = cellfun(@(x)(CheckBins(ELM_STRUCT(i).PosBins,x)),MATCH_SPOTS(:,i),'uniformoutput',false);
-        
+    end
+    if ~isempty(ELM_STRUCT(i).PosPWMs)
         PWMMat = NaN(size(SEQS,1),length(ELM_STRUCT(i).PosPWMs));
         
         for k = 1:length(ELM_STRUCT(i).PosPWMs)
@@ -81,6 +91,7 @@ for i = 1:length(ELM_STRUCT)
            
         end
         ELMPWMScore(:,i)=num2cell(PWMMat,2);
+
     end
 end
 
@@ -91,36 +102,40 @@ for i = 1:length(PAT_STRUCT)
         tempSimple = sum(cell2mat(ELMSimple(SeqInds)),1)>0;
 
         tempBinned = ELMBinned(SeqInds,:);
-        tempBinned = cat(2,tempBinned{:});
-        tempBinned = reshape(tempBinned,nnz(SeqInds),[]);
-        tempBinned = sum(tempBinned,1)>0;
-
+        finalBinned=[];
+        for k=1:size(tempBinned,2)
+            if ~isempty(tempBinned{1,k})
+                finalBinned = [finalBinned sum(cat(1,tempBinned{:,k}),1)>0];
+            end
+        end
+        
         tempPWMScore = ELMPWMScore(SeqInds,:);
-        tempPWMScore = cat(2,tempPWMScore{:});
-        tempPWMScore = reshape(tempPWMScore,nnz(SeqInds),[]);
-        tempPWMScore = max(tempPWMScore,[],1)>0;
+        finalScore=[];
+        for k=1:size(tempPWMScore,2)
+            if ~isempty(tempPWMScore{1,k})
+                finalScore = [finalScore max(cat(1,tempPWMScore{:,k}),[],1)];
+            end
+        end
 
         PAT_STRUCT{i}.ELM_simple = tempSimple;
-        PAT_STRUCT{i}.ELM_vec = tempBinned;
-        PAT_STRUCT{i}.ELM_PWM = tempPWMScore;
+        PAT_STRUCT{i}.ELM_vec = finalBinned;
+        PAT_STRUCT{i}.ELM_PWM = finalScore;
+        PAT_STRUCT{i}.ELM_annot = AllBins;
         
     else
         PAT_STRUCT{i}=rmfield(PAT_STRUCT{i},{'ELM_simple','ELM_vec','ELM_annot'});
     end
-    
-    
-    
-    
+
 end
 
 
 
 
-close(WAITBAR_HANDLE)
+%close(WAITBAR_HANDLE)
 
     function Vals=CheckBins(Bins,MatchedSpots)
         if isempty(MatchedSpots)
-            Vals = false(1,size(Bins,2));
+            Vals = false(1,size(Bins,2)+1);
         else
             Vals = histc(MatchedSpots,[Bins(1,:) Bins(2,end)])>0;
         end
