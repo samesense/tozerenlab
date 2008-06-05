447bc1b9f49625eb508a0a2a7eee5179b0e5a3e4
walldo2 <walldo2@35a78be9-df47-0410-acef-f5dffbb544d5>
2008-06-05 21:33:21 +0000

    pylint updates

--- will/PyUtils/PyUNAFold.py
+++ will/PyUtils/PyUNAFold.py
@@ -3,7 +3,7 @@ import os
 import re
 from numpy import *
 
-def hybrid_min(Seq1,Seq2,Type='RNA',Temp=37,Sodium=1,Magnesium=0,UNAFoldPath='C:\\UNAFold\\bin\\',ReturnType = 'tuple'):
+def hybrid_min(Seq1, Seq2, Type = 'RNA', Temp = 37, Sodium = 1, Magnesium = 0,U NAFoldPath = 'C:\\UNAFold\\bin\\', ReturnType = 'tuple'):
     """
     hybrid_min(Seq1,Seq2)
         Interfaces with the hybrid_min function of UNAFold and returns the free-energy binding of Seq1 with Seq2.
@@ -30,19 +30,19 @@ def hybrid_min(Seq1,Seq2,Type='RNA',Temp=37,Sodium=1,Magnesium=0,UNAFoldPath='C:
     command += Seq1 + ' ' + Seq2
 
 #    print command
-    SysCall = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE)
+    SysCall = subprocess.Popen(command, shell=True, stdout = subprocess.PIPE)
     SysCall.wait()
     output = SysCall.communicate()[0]
 
-    outputList = re.split('\t',output)
+    outputList = re.split('\t', output)
 
-    if ReturnType=='tuple':
-        return (float(outputList[0]),float(outputList[1]),float(outputList[2]))
+    if ReturnType == 'tuple':
+        return (float(outputList[0]), float(outputList[1]), float(outputList[2]))
     elif ReturnType == 'array':
-        return array([float(outputList[0]),float(outputList[1]),float(outputList[2])])
+        return array([float(outputList[0]), float(outputList[1]), float(outputList[2])])
     
 
-def hybrid(Seq1,Seq2,Tmin,Tmax,Tstep=1,Type='RNA',Sodium=1,Magnesium=0,UNAFoldPath='C:\\UNAFold\\bin\\',ReturnType = 'tuple'):
+def hybrid(Seq1, Seq2, Tmin, Tmax, Tstep = 1, Type = 'RNA', Sodium = 1, Magnesium = 0, UNAFoldPath = 'C:\\UNAFold\\bin\\', ReturnType = 'tuple'):
     """
     hybrid(Seq1,Seq2,Tmin,Tmax,Tstep=1)
         Interfaces with the hybrid_min function of UNAFold and returns the free-energy binding of Seq1 with Seq2 across multiple temperature ranges.
@@ -55,8 +55,6 @@ def hybrid(Seq1,Seq2,Tmin,Tmax,Tstep=1,Type='RNA',Sodium=1,Magnesium=0,UNAFoldPa
         UNAFoldPath=['c:\\UNAFold\\bin\\']
         ReturnType=['tuple']|'array'
             Returns the data either as a tuple of numpy array
-
-
     """
 
     command = UNAFoldPath + 'hybrid-min '
@@ -71,22 +69,22 @@ def hybrid(Seq1,Seq2,Tmin,Tmax,Tstep=1,Type='RNA',Sodium=1,Magnesium=0,UNAFoldPa
     command += Seq1 + ' ' + Seq2
 
 #    print command
-    SysCall = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE)
+    SysCall = subprocess.Popen(command, shell=True, stdout = subprocess.PIPE)
     SysCall.wait()
     output = SysCall.communicate()[0]
 
-    outputList = re.split('\n',output)
+    outputList = re.split('\n', output)
 
-    if ReturnType=='tuple':
+    if ReturnType == 'tuple':
         finalList = []
         for rowNum in range(len(outputList)-1):
             temp = re.split('\t',outputList[rowNum])
             print temp
-            finalList.append((Tmin+rowNum*Tstep,float(temp[0]),float(temp[1]),float(temp[2])))
+            finalList.append((Tmin+rowNum*Tstep, float(temp[0]), float(temp[1]), float(temp[2])))
 
         return finalList
-    elif ReturnType=='array':
-        finalArray = zeros((len(outputList)-1,4))
+    elif ReturnType == 'array':
+        finalArray = zeros((len(outputList)-1, 4))
         for rowNum in range(len(outputList)-1):
             temp = re.split('\t',outputList[rowNum])
             finalArray[rowNum] = [Tmin+rowNum*Tstep, float(temp[0]), float(temp[1]), float(temp[2])]
