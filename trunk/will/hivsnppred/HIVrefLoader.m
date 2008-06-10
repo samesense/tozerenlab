6a14554199a6de5a63ef1e0c6741b18ce7d261a2
walldo2 <walldo2@35a78be9-df47-0410-acef-f5dffbb544d5>
2008-06-10 21:45:07 +0000

    Changed HIVrefLoader.m so now it can load everything from a Genbank formatted file instead of an ad-hoc created fasta file ... it can also take a file-array and return a struct-array of HIV_REF objects ... a setup for using multiple reference samples in the alignments

--- trunk/will/hivsnppred/HIVrefLoader.m
+++ trunk/will/hivsnppred/HIVrefLoader.m
@@ -1,4 +1,4 @@
-function HIV_REF=HIVrefLoader(FILENAME)
+function HIV_REF=HIVrefLoader(FILENAME,SubInput,SkipBads)
 %   HIVrefLoader
 %       HIVrefLoader reads a FASTA formatted file which has the complete
 %       HIV genomic region followed by the the NT sequence of each protien
@@ -9,16 +9,105 @@ function HIV_REF=HIVrefLoader(FILENAME)
 %
 %
 
-[headers seqs]=fastaread(FILENAME);
-
-gene_names=cell(length(headers)-1,1);
-locs=zeros(length(headers)-1,2);
-for i=2:length(headers)
-    temp=textscan(headers{i},'gi|4558520:%n-%n %s HIV-1, complete genome');
-    locs(i-1,:)=[temp{1:2}];
-    gene_names(i-1)=temp{3};
+if iscell(FILENAME)&&length(FILENAME)>1
+    HIV_REF = [];
+    
+    for i = 1:length(FILENAME)
+        HIV_REF = [HIV_REF; HIVrefLoader(FILENAME{i},SubInput{i},SkipBads)];
+    end
+    
+    
+else
+    testData = genbankread(FILENAME);
+    [gene_names{1:length(testData.CDS)}] = deal(testData.CDS(:).gene);
+    [product_names{1:length(testData.CDS)}] = deal(testData.CDS(:).product);
+    [notes{1:length(testData.CDS)}] = deal(testData.CDS(:).note);
+    
+    [standardizedNames cannotDefine] = nameMapping(gene_names,product_names,notes);
+    
+    if SkipBads&&any(cannotDefine)
+        HIV_REF=[];
+        return
+    end
+    
+    [trans_aa{1:length(testData.CDS)}] = deal(testData.CDS(:).translation);
+    [locs{1:length(testData.CDS)}] = deal(testData.CDS(:).indices);
+    locsval = cell2mat(cellfun(@(x)(x(1:2)),locs','uniformoutput',false));
+    HIV_REF=struct('Subtype',SubInput,'name',testData.LocusName,...
+        'Sequence',upper(testData.Sequence),...
+        'GeneNames',{standardizedNames(~cannotDefine)'},'AAseqs',{trans_aa(~cannotDefine)'},...
+        'GenePos',locsval(~cannotDefine,:),'AppendedGenome',[trans_aa{~cannotDefine}],...
+        'AAPos',cumsum([1 cellfun('length',trans_aa(~cannotDefine(1:end-1)))]));
 end
 
-trans_aa=nt2aa(seqs(2:end));
+    function [standardNames junked] = nameMapping(genes_annot,product_annot,notes_annot)
+        standardNames = cell(size(genes_annot));
+        junked = false(size(genes_annot));
+        
+        known_names{1} = 'gag';
+        known_names{2} = 'pol[^y]';
+        known_names{3} = 'vif';
+        known_names{4} = 'vpr';
+        known_names{5} = 'tat';
+        known_names{6} = 'rev';
+        known_names{7} = 'vpu';
+        known_names{8} = 'env';
+        known_names{9} = 'nef';
+        
+        strs = cat(2,genes_annot',product_annot');
+        strs(cellfun('isempty',strs)) = {' '};
+               
+        possible_choices = regexpi(strs,'\w*','match');
 
-HIV_REF=struct('Sequence',seqs{1},'GeneNames',{gene_names},'AAseqs',{trans_aa'},'GenePos',locs,'AppendedGenome',[trans_aa{:}],'AAPos',cumsum([1 cellfun('length',trans_aa(1:end-1))]));
+        for i = 1:size(possible_choices,1)
+            temp = cat(2,possible_choices{i,:});
+            if isempty(temp)
+                continue
+            end
+            tempStrs = cell(1,2*size(temp,2));
+            tempStrs(:) = {' '};
+            tempStrs(1:2:end) = temp(:);
+           
+            pos=regexpi(cat(2,tempStrs{:}),known_names,'start');
+            
+            if ~all(cellfun('isempty',pos))&&sum(~cellfun('isempty',pos))==1
+                standardNames{i}=known_names{find(~cellfun('isempty',pos),1)}(1:3);
+            end
+        end
+        
+        %second pass
+        
+        strs = cat(2,genes_annot',product_annot',notes_annot');
+        strs(cellfun('isempty',strs)) = {' '};
+        strs(:,3) = cellfun(@(x)(x(1,:)),strs(:,3),'uniformoutput',false);
+                
+        possible_choices = regexpi(strs,'\w*','match');
+        
+        for i = 1:size(possible_choices,1)
+            if isempty(standardNames{i})
+                temp = cat(2,possible_choices{i,:});
+                tempStrs = cell(1,2*size(temp,2));
+                tempStrs(:) = {' '};
+                tempStrs(1:2:end) = temp(:);
+
+                pos=regexpi(cat(2,tempStrs{:}),known_names,'start');
+
+                if all(cellfun('isempty',pos))
+                    display('Could not find any in:')
+                    display(cat(2,tempStrs{:}))
+                    junked(i)=true;
+                elseif sum(~cellfun('isempty',pos))>1
+                    display('Found multiple in:')
+                    display(cat(2,tempStrs{:}))
+                    display(cat(2,known_names{~cellfun('isempty',pos)}))
+                    junked(i)=true;
+                else
+
+                    standardNames{i}=known_names{find(~cellfun('isempty',pos),1)}(1:3);
+                end
+            end
+        end
+
+        
+    end
+end
