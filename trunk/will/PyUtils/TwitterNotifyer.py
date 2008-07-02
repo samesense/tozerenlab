a73895ba9711de10e066687af225825d812e505b
walldo2 <walldo2@35a78be9-df47-0410-acef-f5dffbb544d5>
2008-07-02 22:21:33 +0000

    made pylint recomended changes

--- trunk/will/PyUtils/TwitterNotifyer.py
+++ trunk/will/PyUtils/TwitterNotifyer.py
@@ -1,3 +1,18 @@
+"""
+TwitterNotifyer.py
+    A set of methods which can notify a twitter account with the computation
+    progress.
+
+import TwitterNotifyer
+
+with TwitterNotify.NotifContext(USER = 'T_User', PASSWORD = 'T_Pass') as notif:
+    Do stuff ....
+    #you can send updates
+    notif.UpdateMessage('Got this Far')
+
+
+"""
+
 import twitter
 import threading
 import time
@@ -5,88 +20,87 @@ from collections import deque
 import logging
 import getpass
 
-
-
-logging.basicConfig(level=logging.DEBUG,format='[%(threadName)-10s] %(message)s %(asctime)s')
-
+logging.basicConfig(level = logging.DEBUG,
+                    format = '[%(threadName)-10s] %(message)s %(asctime)s')
 
 class NotifContext:
     def __enter__(self):
         """
         __enter__
-            Called on entry into the "with" statement.  Starts the PaceThread.
+            Called on entry into the "with" statement.  Starts the pace_thread.
         """
-        #cont = NotifContext()
 
-        self.DoAnnounce(self.DefStartAnnounce)
-        self.PaceThread = threading.Thread(name='PaceThread',target=self.PaceAnnounce)
-        self.PaceThread.start()
+        self.DoAnnounce(self.default_start_ann)
+        
+        self.pace_thread.start()
         return self
 
 
-    def __init__(self,method='twitter',user=None,password=None):
+    def __init__(self, METHOD = 'twitter', USER = None, PASSWORD = None):
 
-        if (method == 'twitter') & (user == None):
-            user = getpass.getpass(promt = 'Twitter Account Name: ')
+        if (METHOD == 'twitter') & (USER == None):
+            self.user = getpass.getpass(promt = 'Twitter Account Name: ')
 
-        if (method == 'twitter') & (password == None):
-            password = getpass.getpass(prompt = 'Twitter password for ' + user)
+        if (METHOD == 'twitter') & (PASSWORD == None):
+            self.password = getpass.getpass(prompt = 'Passw for ' + self.user)
 
 
         
         #logging.debug('Initializing')
-        self.tweetBird = twitter.Api(username = user, password = password)
-        self.user=user
-        self.password = password
+        self.tweet_bird = twitter.Api(username = self.user,
+                                      password = self.password)
 
-        self.method = method
+        self.method = METHOD
 
         if self.method == 'test':
-            self.updateTimeLimit = 60
+            self.update_time_limit = 60
         else:
-            self.updateTimeLimit = 60*60*2
+            self.update_time_limit = 60*60*2
             
-        
-        self.DefStartAnnounce = 'Starting Computation'
-        self.DefContAnnounce = 'Still Computing'
-        self.DefFinished = 'Done Computing'
+        self.default_start_ann = 'Starting Computation'
+        self.default_cont_ann = 'Still Computing'
+        self.default_finished = 'Done Computing'
 
-        self.ThisContAnnounce = deque()
-        self.ThisContAnnounce.append(self.DefContAnnounce)
+        self.this_cont_ann = deque()
+        self.this_cont_ann.append(self.default_cont_ann)
 
-        self.finishedRunning = threading.Event()
+        self.finished_running = threading.Event()
 
-        self.MaxTry = 5
+        self.max_try = 5
     
-    
+        self.pace_thread = threading.Thread(name = 'pace_thread',
+                                            target = self.PaceAnnounce)
         
-    def __exit__(self,typeIn,valueIn,tracebackIn):
+    def __exit__(self, TYPE_IN, VALUE_IN, TRACEBACK_IN):
         """
         __exit__
-            Called when the main function has ended.  This sets the self.finishedRunning Event,
-        .join()'s the pacing thread and then sends outputs based on whether the function
-        errored or finished properly.
+            Called when the main function has ended.  This sets the
+        self.finished_running Event, .join()'s the pacing thread and then sends
+        outputs based on whether the function errored or finished properly.
         """
+
         #logging.debug('Made to Exit')
 
-        #Set event so PaceThread will exit next time it checks
-        self.finishedRunning.set()
+        #Set event so pace_thread will exit next time it checks
+        self.finished_running.set()
 
         #while waiting, send out final message
-        if typeIn == None:
+        if TYPE_IN == None:
             self.DoAnnounce('Finished Computation')
         else:
-            self.DoAnnounce('Something Bad Happened:' + str(typeIn))
+            self.DoAnnounce('Something Bad Happened:' + str(TYPE_IN))
 
-        #wait until the thread exits otherwise you get crazyness when python exits
-        self.PaceThread.join()
+        #wait until the thread exits otherwise you get
+        #crazyness when python exits
+        self.pace_thread.join()
 
         
 
-    def start(self):
+    def Start(self):
         """
-        start
-            Calls the .__enter__ method, allows easy usage with pre-"with" versions of python
+        Start
+            Calls the .__enter__ method, allows easy usage with pre-"with"
+            versions of python
         """
 
         self.__enter__()
@@ -96,50 +110,55 @@ class NotifContext:
     def PaceAnnounce(self):
         """
         PaceAnnounce
-            Acts as a thread which keeps the time and sends periodic calls of DoAnnounce.
-        It continually checks self.finishedRunning to see if the function has ended.
-
+            Acts as a thread which keeps the time and sends periodic calls of
+            DoAnnounce.  It continually checks self.finished_running to see if
+            the function has ended.
 
         """
+        
         #logging.debug('In PaceAnnouce')
-        while not(self.finishedRunning.isSet()):
+        while not(self.finished_running.isSet()):
             #wait until the Event has been triggered or until update limit is up
-            self.finishedRunning.wait(self.updateTimeLimit)
+            self.finished_running.wait(self.update_time_limit)
             
-            if not(self.finishedRunning.isSet()):
-                #if the time-limit is up and the Event hasn't been .set() then display an update message
-                self.currentWaitTime=0
-                self.DoAnnounce(self.ThisContAnnounce.pop())
-                if len(self.ThisContAnnounce) == 0:
-                    self.ThisContAnnounce.append(self.DefContAnnounce)
+            if not(self.finished_running.isSet()):
+                #if the time-limit is up and the Event hasn't been .set() then
+                #display an update message
+                self.DoAnnounce(self.this_cont_ann.pop())
+                if len(self.this_cont_ann) == 0:
+                    self.this_cont_ann.append(self.default_cont_ann)
 
 
-    def UpdateMessage(self,message,now=False):
+    def UpdateMessage(self, MESSAGE, NOW = False):
         """
         UpdateMessage
-            This allows the program to send a status updates based on the progress of the function.
+            This allows the program to send a status updates based on the
+            progress of the function.
 
         UpdateMessage(message, now=False)
-            If now is False then the message will be sent out at the next scheduled update.  If now
-        is True then the timer is reset and the message will be sent immediately.
+            If now is False then the message will be sent out at the next
+            scheduled update.  If now is True then the timer is reset and the
+            message will be sent immediately.
 
 
         """
 
-        if now:
-            self.DoAnnounce(message)
+        if NOW:
+            self.DoAnnounce(MESSAGE)
         else:
-            self.ThisContAnnounce.append(message)
+            self.this_cont_ann.append(MESSAGE)
         
            
 
-    def DoAnnounce(self,message):
+    def DoAnnounce(self, MESSAGE):
         """
         DoAnnouce
-            Sends the provided message through the desired channel ... either Twitter or 'Logging'
+            Sends the provided message through the desired channel ... either
+            Twitter or 'Logging'
 
-        DoAnnouce(self,message)
-            Sends the provided message.  Will retry self.MaxTry times incase twitter is unresponsive
+        DoAnnouce(self, MESSAGE)
+            Sends the provided message.  Will retry self.max_try times incase
+            twitter is unresponsive
 
         Returns:
             True|False
@@ -147,12 +166,12 @@ class NotifContext:
 
         """
         if self.method == 'test':
-            logging.info('Sending Tweet: ' + message)
+            logging.info('Sending Tweet: ' + MESSAGE)
             return True
         elif self.method == 'twitter':
-            for i in xrange(self.MaxTry):
+            for i in xrange(self.max_try):
                 try:
-                    self.tweetBird.PostUpdate(message)
+                    self.tweet_bird.PostUpdate(MESSAGE)
                     return True
                 except:
                     time.sleep(15)
@@ -160,24 +179,26 @@ class NotifContext:
 
 
 
-def testNotif(notif,waittime=20,numReps=10,updateiter=4,erroriter=-1):
+def testNotif(NOTIF, WAIT_TIME = 20, NUM_REPS = 10, UPDATE_ITER = 4,
+              ERROR_ITER = -1):
     """
     An example snipet of code.
     """
     
     #with NotifContext(method='twitter') as notif:
-    for i in range(numReps):
+    for i in range(NUM_REPS):
         #do some code ...
         print i
-        if i==updateiter:
+        if i == UPDATE_ITER:
             #send an update NOW
-            notif.UpdateMessage('Update message',now=True)
-        elif i==updateiter+1:
+            NOTIF.UpdateMessage('Update message', now = True)
+
+        elif i == UPDATE_ITER+1:
             #send an update at the next possible occasion
-            notif.UpdateMessage('Wait Update message')
-        elif i==erroriter:
+            NOTIF.UpdateMessage('Wait Update message')
+
+        elif i == ERROR_ITER:
             #give an error
             raise IndexError
         else:
-            
-           time.sleep(waittime)
+            time.sleep(WAIT_TIME)
