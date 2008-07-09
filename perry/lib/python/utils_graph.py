8bff4a2254cf687d81058fd2a31d095548b6a4a6
samesense <samesense@35a78be9-df47-0410-acef-f5dffbb544d5>
2008-07-09 22:32:04 +0000

    makin pretty code

--- perry/lib/python/utils_graph.py
+++ perry/lib/python/utils_graph.py
@@ -1,23 +1,54 @@
+#----------------------------------------
+#
+# Author     Perry Evans
+#            evansjp@mail.med.upenn.edu
+# 2008
+#----------------------------------------
+"""
+Functions here help with network loading,
+printing, and searching.
+"""
 import string
 
 def getNodes(afile):
+    """ Make a {} from a file.  Each line
+        of the file is one item in the {}.
+    
+    @param afile: file w/ one item per line
+    @return: {} of nodes
+    """
+
     nodes = dict()
     f = open(afile)
-    for line in f.xreadlines():
+    for line in f:
         nodes[ line.strip() ] = True
     f.close()
     return nodes
 
 def dumpNodes(afile, ls):
+    """ Output {} to file.  One item
+    per line.
+
+    @param afile: write to this file
+    @param ls: {} with items for writing
+    """
+
     f = open(afile, 'w')
     for g in ls.keys():
         f.write(g + '\n')
     f.close()
 
 def getEdges(afile):
+    """ Make edge {} from file.
+    {} format is d[n1][n2] = True
+
+    @param afile: tab-delimited file; one edge per line; edges are bi-directional
+    @return: {} of edges
+    """
+
     edges = dict()
     f = open(afile)
-    for line in f.xreadlines():
+    for line in f():
         [n1, n2] = map(string.strip, line.split('\t'))
         if not edges.has_key(n1): edges[n1] = dict()
         if not edges.has_key(n2): edges[n2] = dict()
@@ -27,6 +58,13 @@ def getEdges(afile):
     return edges
 
 def dumpEdges(afile, d):
+    """ Write edges to file.
+    
+    @param afile: output is tab-delimited.
+    bi-directional edges are not duplicated
+    @param d: {} format is d[n1][n2] = True
+    """
+    
     f = open(afile, 'w')
     seen = {}
     for g1 in d.keys():
@@ -40,6 +78,12 @@ def dumpEdges(afile, d):
     f.close()
 
 def mkNodesFromEdges(afile):
+    """ Given an edge file, make a {} of nodes.
+
+    @param afile: tab-delimted file of edges; one per line; edges are bi-directional
+    @return: {} of nodes
+    """
+
     nodes = {}
     f = open(afile)
     for line in f.xreadlines():
@@ -50,6 +94,14 @@ def mkNodesFromEdges(afile):
     return nodes
 
 def getConnected(ls_file, net_file):
+    """ Find all the genes in the network
+    that are one away from these genes.
+
+    @param ls_file: file of genes to find connections to; one gene per line
+    @param net_file: tab-delimited file for the network; one edge per line; edges are bi-directional
+    @return: {} of genes
+    """
+
     ls = getNodes(ls_file)
     net = getEdges(net_file)
     second_level_ls = {}
@@ -61,6 +113,13 @@ def getConnected(ls_file, net_file):
     return second_level_ls
 
 def intersectLists(list_of_lists):
+    """ Given a list of {}, return the intersection
+    as a {}.
+
+    @param list_of_lists: [] of {}
+    @return: {} of genes
+    """
+
     same = {}
     for s in list_of_lists[0].keys():
         count = 0
@@ -71,19 +130,39 @@ def intersectLists(list_of_lists):
     return same
 
 def unionLists(list_of_lists):
+    """ Given a list of {}, return the union
+    as a {}.
+
+    @param list_of_lists: list of {}
+    @return: {} of union
+    """
+
     all = {}
     for ls in list_of_lists:
         for k in ls.keys():
             all[k] = True
     return all
 
-# get degree
 def degree(gene, network):
+    """ Find the degree of this node
+    in this network.
+    
+    @param gene: node in the network
+    @param network: edge {} format d[n1][n2] = True
+    @return: deg of gene as int
+    """
+
     return len(network[gene].keys())
 
-# given a set of genes
-# return their average connectivity
 def avgConnectivity(geneLs, network):
+    """ Given a set of genes, return
+    their average connectivity in the network.
+
+    @param geneLs: {} of genes
+    @param network: edge {} format d[n1][n2] = True
+    @return: float for avg connectivity
+    """
+
     [connectivity, total] = [0, 0]
     for gene in geneLs.keys():
         if network.has_key(gene):
