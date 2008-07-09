da5aecc5773e4be8cd08cef9dc946fc299647413
samesense <samesense@35a78be9-df47-0410-acef-f5dffbb544d5>
2008-07-09 22:32:04 +0000

    makin pretty code

--- trunk/perry/lib/python/utils_scripting.py
+++ trunk/perry/lib/python/utils_scripting.py
@@ -1,6 +1,26 @@
+#-----------------------------------------
+#  Author:     Perry Evans
+#              evansjp@mail.med.upenn.edu
+#  2008
+#-----------------------------------------
+"""
+Functions in this module aid in writing
+small scripts
+"""
 import sys
 
-def checkStart(sys_args, req_args, examples):
+def checkstart(sys_args, req_args, examples):
+    """Place this at the start of cmd line scripts to check for
+       the correct # of script argumtns.  If you do not recieve
+       the correct arguments, the script exits and suggests 
+       which arguments should be used.
+
+    @param sys_args: sys.argv
+    @param req_args: list of the arguments you want for the 
+                     script
+    @param examples: list of examples arguments for the script
+    """
+
     if not len(sys_args) == len(req_args) + 1:
         print 'ENTER'
         for arg in req_args:
