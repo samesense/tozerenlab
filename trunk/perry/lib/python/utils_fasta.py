bf8e0b17e58013289d350e7d84e17365a25f7907
samesense <samesense@35a78be9-df47-0410-acef-f5dffbb544d5>
2008-07-10 13:23:00 +0000

    fixed pylint probs

--- trunk/perry/lib/python/utils_fasta.py
+++ trunk/perry/lib/python/utils_fasta.py
@@ -1,38 +1,71 @@
-# given a list of genes
-# and a FASTA file,
-# print the fasta entries
-def printFASTA_forGenes(geneLsFile, FASTA_file, output_file):
-    genes = {}
-    f = open(genesFile)
-    for line in f.xreadlines():
-        genes[line.strip()] = True
-    f.close()
+#-----------------------------------------
+#  Author:     Perry Evans
+#              evansjp@mail.med.upenn.edu
+#  2008
+#
+#-----------------------------------------
+"""
+Functions for dealing with FASTA files.  The
+format I'm using is:
 
-    f = open(FASTA_file)
+>gene_name
+seq
+seq
+seq
+... .
+"""
+
+def printFASTA_forGenes(gene_file, fasta_file, output_file):
+    """ Given a file of genes and a FASTA file,
+        print the fasta entries in the gene file.
+    
+    @param gene_file: one gene per line; you want seqs for these genes
+    @param fasta_file: seqs
+    @param output_file: where to stick the seqs
+    """
+    
+    genes = {}
+    gene_f = open(gene_file)
+    for line in gene_f:
+        genes[line.strip()] = True
+    gene_f.close()
+
+    fasta_f = open(fasta_file)
     fout = open(output_file, 'w')
-    line = f.readline()
+    line = fasta_f.readline()
     while line != '':
         if line[0] == '>':
-            if have.has_key(line[1:].strip()):
+            if genes.has_key(line[1:].strip()):
                 fout.write(line)
-                line = f.readline()
+                line = fasta_f.readline()
                 while line[0] != '>':
                     fout.write(line)
-                    line = f.readline()
-                    if line == '': break
+                    line = fasta_f.readline()
+                    if line == '':
+                        break
             else:
-                line = f.readline()
+                line = fasta_f.readline()
                 while line[0] != '>':
-                    line = f.readline()
-                    if line == '': break
-    f.close()
+                    line = fasta_f.readline()
+                    if line == '':
+                        break
+    fasta_f.close()
     fout.close()
 
 def loadFASTA(fasta_file):
+    """ Make a {} from a FASTA file.
+
+    @param fasta_file: >name
+                       seq
+                       seq ...
+    @return: fasta[gene] = seq (oneline)
+    """
+
     fasta = {}
-    f = open(fasta_file)
+    fasta_f = open(fasta_file)
     name = ''
-    line = f.readline()
+    seq = ''
+    line = fasta_f.readline()
     while line != '':
         if line[0] == '>':
             if name != '':
@@ -41,15 +74,23 @@ def loadFASTA(fasta_file):
             seq = ''
         else:
             seq = seq + line.strip()
-        line = f.readline()
-    f.close()
+        line = fasta_f.readline()
+    fasta_f.close()
 
     if name != '':    
-        fasta[name ] = seq
+        fasta[name] = seq
 
     return fasta
 
 def prettyPrint_runner(geneid, seq_string):
+    """ Return FASTA formatted version of 
+        this gene and its sequence.
+
+    @param geneid: gene_name
+    @param seq_string: FASTA seq on one line w/ no whitespace
+    @return: formatted FASTA version
+    """
+
     breakcount = 50
     line = '>' + geneid + '\n'
     while len(seq_string) > breakcount:
@@ -60,7 +101,21 @@ def prettyPrint_runner(geneid, seq_string):
     return line
 
 def prettyPrint(geneid, seq_string):
+    """ Print FASTA formatted version of this
+        gene and its sequence.
+
+    @param geneid: gene_name
+    @param seq_string: FASTA seq on one line w/ no whitespace
+    """
+
     print prettyPrint_runner(geneid, seq_string)
 
-def prettyPrint_file(geneid, seq_string, file):
-    file.write( prettyPrint_runner(geneid, seq_string) + '\n')
+def prettyPrint_file(geneid, seq_string, afile):
+    """ Write FASTA formatted gene/seq to this open file.
+
+    @param geneid: gene_name
+    @param seq_string: FASTA seq on one line w/ no whitespace
+    @param afile: an open output file
+    """
+    
+    afile.write( prettyPrint_runner(geneid, seq_string) + '\n')
