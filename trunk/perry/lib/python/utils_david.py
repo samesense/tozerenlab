1ae50ff01c0769e717bcfd7d2d0d9543ebeacd48
samesense <samesense@35a78be9-df47-0410-acef-f5dffbb544d5>
2008-07-10 12:52:06 +0000

    implemented pylint suggestions

--- trunk/perry/lib/python/utils_david.py
+++ trunk/perry/lib/python/utils_david.py
@@ -5,14 +5,12 @@
 #
 #-----------------------------------------
 """
-Functions for automating calls to DAVID
+Functions for automating calls to DAVID.
 """
-import sys
 import PyMozilla
-import re
 
 def fullMonty(astring):
-    """ Remove commas and whitespace from
+    """ Remove hsa, commas, and whitespace from
         this term.  This only works for
         human proteins.
 
@@ -20,7 +18,8 @@ def fullMonty(astring):
     @return: formatted string
     """
 
-    if astring == '': return astring
+    if astring == '':
+        return astring
     if astring[-1] == 'O':
         astring = astring[0:-4]
     elif astring[-8:-5] == 'hsa':
@@ -37,19 +36,22 @@ def cat2term2protein_sig(iterable):
 
     cat2term2protein = {}
     for line in iterable:
-         [Category, Term, Count, Percent,
-         PValue, Genes, ListTotal,
-         PopHits, PopTotal, FoldChange] = [x.strip for x in  line.split('\t')]
-        if not d.has_key(Category):
-            cat2term2protein[Category] = {}
-        genes = Genes.strip(',').split(',')
-        geneLs = {}
+        splitup = [x.strip for x in  line.split('\t')]
+        category = splitup[0]
+        term = splitup[1]
+        count = splitup[2]
+        pvalue = splitup[4]
+        genes = splitup[5]         
+        if not cat2term2protein.has_key(category):
+            cat2term2protein[category] = {}
+        genes = genes.strip(',').split(',')
+        gene_d = {}
         for gene in genes: 
-            geneLs[gene.strip().lstrip()] = True
-        cat2term2protein[Category][Term] = {}
-        cat2term2protein[Category][Term]['count'] = int(Count)
-        cat2term2protein[Category][Term]['pvalue'] = float(PValue)
-        cat2term2protein[Category][Term]['genes'] = geneLs
+            gene_d[gene.strip().lstrip()] = True        
+        cat2term2protein[category][term] = {}
+        cat2term2protein[category][term]['count'] = int(count)
+        cat2term2protein[category][term]['pvalue'] = float(pvalue)
+        cat2term2protein[category][term]['genes'] = gene_d
     return cat2term2protein
 
 def cat2term2protein_sigFile(afile):
@@ -59,10 +61,10 @@ def cat2term2protein_sigFile(afile):
     @return: [Category][Term]['count' | 'pvalue' | 'genes'={} ]
     """
 
-    f_DAVID = open(afile)
-    f_DAVID.readline()
-    cat2term2protein = cat2term2protein_sig(f)
-    f_DAVID.close()
+    f_david = open(afile)
+    f_david.readline()
+    cat2term2protein = cat2term2protein_sig(f_david)
+    f_david.close()
     return cat2term2protein
 
 def cat2term2protein_all(iterable):
@@ -74,21 +76,23 @@ def cat2term2protein_all(iterable):
     """
 
     cat2term2protein = {}
-    Categories = [x.strip for x in iterable[0].split('\t')[3:7]]
-    for cat in Categories: cat2term2protein[cat] = {}
+    categories = [x.strip for x in iterable[0].split('\t')[3:7]]
+    for cat in categories:
+        cat2term2protein[cat] = {}
     for line in iterable[1:]:
-        sp = [x.strip for x in line.split('\t')]
-        for i in xrange(len(Categories)):
-            cat = Categories[i]
-            terms = [x.strip for x in sp[i+3].strip(',').split(':')]
-            for x in xrange(len(terms)-1):
-                if terms[x] != '':
-                    term = terms[x].split()[-1]+':'+fullMonty(terms[x+1])
+        splitup = [x.strip for x in line.split('\t')]
+        for i in xrange(len(categories)):
+            cat = categories[i]
+            terms = [x.strip for x in splitup[i+3].strip(',').split(':')]
+            for index in xrange(len(terms)-1):
+                if terms[index] != '':
+                    term = terms[index].split()[-1] + ':'\
+                         + fullMonty(terms[index+1])
                     if not cat2term2protein[cat].has_key(term):
                         cat2term2protein[cat][term] = {}
                         cat2term2protein[cat][term]['count'] = 0
                         cat2term2protein[cat][term]['genes'] = {}
-                    cat2term2protein[cat][term]['genes'][sp[0]] = True
+                    cat2term2protein[cat][term]['genes'][splitup[0]] = True
                     cat2term2protein[cat][term]['count'] += 1               
     return cat2term2protein
 
@@ -99,9 +103,9 @@ def cat2term2protein_allFile(afile):
     @return: [Category][Term]['count' | 'genes'={} ]
     """
 
-    f_DAVID = open(afile)
-    cat2term2protein = cat2term2protein_all(f_DAVID)
-    f_DAVID.close()
+    f_david = open(afile)
+    cat2term2protein = cat2term2protein_all(f_david)
+    f_david.close()
     return cat2term2protein
 
 def getSigKEGGgenes_sigFile(afile, cutoff):
@@ -118,24 +122,24 @@ def getSigKEGGgenes_sigFile(afile, cutoff):
     cat2term2protein = cat2term2protein_sigFile(afile)
     for path in cat2term2protein['KEGG_PATHWAY'].keys():
         if cat2term2protein['KEGG_PATHWAY'][path]['pvalue'] < cutoff:
-            for g in cat2term2protein['KEGG_PATHWAY'][path]['genes'].keys():
-                genes[g] = True
+            for gene in cat2term2protein['KEGG_PATHWAY'][path]['genes'].keys():
+                genes[gene] = True
     return genes
 
-def getSigKEGGgenes(motifSearchResults, bg, cutoff):
+def getSigKEGGgenes(motif_search_results, background, cutoff):
     """ Given a foreground and background {}, find
         significant KEGG pathways with DAVID and return
         {} of foreground genes that are in these pathways
         for the given p-value cutoff.
         
-    @param motifSearchResults: foreground {}
-    @param bg: background {}
+    @param motif_search_results: foreground {}
+    @param backgground: background gene {}
     @param cuttoff: float p-value cutoff
     @return: {} of foreground genes in sig KEGG pathways
     """
 
     genes = {}
-    cat2term2protein = getSigTerms(motifSearchResults, bg)
+    cat2term2protein = getSigTerms(motif_search_results, background)
     for path in cat2term2protein['KEGG_PATHWAY'].keys():
         if cat2term2protein['KEGG_PATHWAY'][path]['pvalue'] < cutoff:
             for gene in cat2term2protein['KEGG_PATHWAY'][path]['genes'].keys():
@@ -154,69 +158,99 @@ def getSigKEGGgenes(motifSearchResults, bg, cutoff):
 #            genes[gene] = True
 #    return genes
 
-def getAllTerms(geneFile):
-    MozEmu = PyMozilla.MozillaEmulator(cacher=None,trycount=0)
+def getAllTerms(gene_file):
+    """ Given a file with one gene per line, return
+        the parsed DAVID annotation for the genes in
+        the form [Category][Term]['count' | 'genes'={} ].
+    
+    @param gene_file: one gene per line in this file
+    @return [Category][Term]['count' | 'genes'={} ]
+    """
+
+    david_prefix = 'http://david.abcc.ncifcrf.gov/'
+    moz_emu = PyMozilla.MozillaEmulator(cacher=None, trycount=0)
     files = [['fileBrowser', 'testData', '']]
     fields = []
-    MozEmu.download('http://david.abcc.ncifcrf.gov/tools.jsp')
-    fdata = file(geneFile).read()
+    moz_emu.download(david_prefix + 'tools.jsp')
+    fdata = file(gene_file).read()
     fields = []
     fields.append(['idType', 'ENTREZ_GENE_ID'])
     fields.append(['Mode', 'file'])
-    fields.append(['uploadType','list'])
+    fields.append(['uploadType', 'list'])
 
     files = []
     files.append(['fileBrowser', 'testData', fdata])
     
-    ans = MozEmu.post_multipart('http://david.abcc.ncifcrf.gov/tools.jsp', fields, files)
-    
-    david = MozEmu.download('http://david.abcc.ncifcrf.gov/template.css')
-    david = MozEmu.download('http://david.abcc.ncifcrf.gov/scripts/sidebar.js')
-    david = MozEmu.download('http://david.abcc.ncifcrf.gov/scripts/summary.js')
-    userDownloadPage = MozEmu.download('http://david.abcc.ncifcrf.gov/annotationReport.jsp?annot=,GOTERM_BP_5,GOTERM_CC_5,GOTERM_MF_5,KEGG_PATHWAY&currentList=0')
-    index = userDownloadPage.find('UserDownload')
-    addr = userDownloadPage[index+13:].split('.')[0]
-    return cat2term2protein_all(MozEmu.download('http://david.abcc.ncifcrf.gov/UserDownload/' + addr  + '.txt').split('\n')[1:-1])
+    moz_emu.post_multipart(david_prefix + 'tools.jsp',
+                           fields, files)
+    david_cats = 'GOTERM_BP_5,GOTERM_CC_5,GOTERM_MF_5,KEGG_PATHWAY'
+    moz_emu.download(david_prefix + 'template.css')
+    moz_emu.download(david_prefix + 'scripts/sidebar.js')
+    moz_emu.download(david_prefix + 'scripts/summary.js')
+    user_download_page = moz_emu.download(david_prefix
+                                          + 'annotationReport.jsp?annot=,'
+                                          + david_cats + '&currentList=0')
+    index = user_download_page.find('UserDownload')
+    addr = user_download_page[index+13:].split('.')[0]
+    return cat2term2protein_all(moz_emu.download(david_prefix
+                                                 + 'UserDownload/'
+                                                 + addr 
+                                                 + '.txt').split('\n')[1:-1])
 
-def getSigTerms(fgFile, bgFile):
-    MozEmu = PyMozilla.MozillaEmulator(cacher=None,trycount=0)
-    #try:
+def getSigTerms(fg_file, bg_file):
+    """ Given a file with one gene per line, return
+        the parsed DAVID significance chart for the genes in
+        the form [Category][Term]['count' | 'genes'={} ].
+    
+    @param fg_file: one gene per line in this file
+    @param bg_file: one gene per line; backround gene set
+    @return [Category][Term]['count' | 'p-value' | 'genes'={} ]
+    """
+
+    david_prefix = 'http://david.abcc.ncifcrf.gov/'
+    david_cats = 'GOTERM_BP_5,GOTERM_CC_5,GOTERM_MF_5,KEGG_PATHWAY'
+    moz_emu = PyMozilla.MozillaEmulator(cacher=None, trycount=0)
     files = [['fileBrowser', 'testData', '']]
     fields = []
-    MozEmu.download('http://david.abcc.ncifcrf.gov/tools.jsp')
-    fdata = file(fgFile).read()
+    moz_emu.download(david_prefix + 'tools.jsp')
+    fdata = file(fg_file).read()
     fields = []
     fields.append(['idType', 'ENTREZ_GENE_ID'])
     fields.append(['Mode', 'file'])
-    fields.append(['uploadType','list'])
+    fields.append(['uploadType', 'list'])
 
     files = []
     files.append(['fileBrowser', 'testData', fdata])
     
-    ans = MozEmu.post_multipart('http://david.abcc.ncifcrf.gov/tools.jsp', fields, files)
+    moz_emu.post_multipart(david_prefix + 'tools.jsp',
+                           fields, files)
     
-    david = MozEmu.download('http://david.abcc.ncifcrf.gov/template.css')
-    david = MozEmu.download('http://david.abcc.ncifcrf.gov/scripts/sidebar.js')
-    david = MozEmu.download('http://david.abcc.ncifcrf.gov/scripts/summary.js')
+    moz_emu.download(david_prefix +  'template.css')
+    moz_emu.download(david_prefix + 'scripts/sidebar.js')
+    moz_emu.download(david_prefix + 'scripts/summary.js')
 
     # upload the background
-    fdata = file(bgFile).read()
+    fdata = file(bg_file).read()
     fields = []
     fields.append(['idType', 'ENTREZ_GENE_ID'])
     fields.append(['Mode', 'file'])
-    fields.append(['uploadType','population'])
+    fields.append(['uploadType', 'population'])
 
     files = []
     files.append(['fileBrowser', 'testData', fdata])
     
-    ans = MozEmu.post_multipart('http://david.abcc.ncifcrf.gov/tools.jsp', fields, files)
+    moz_emu.post_multipart('http://david.abcc.ncifcrf.gov/tools.jsp',
+                           fields, files)
     
-    david = MozEmu.download('http://david.abcc.ncifcrf.gov/template.css')
-    david = MozEmu.download('http://david.abcc.ncifcrf.gov/scripts/sidebar.js')
-    david = MozEmu.download('http://david.abcc.ncifcrf.gov/scripts/summary.js')
+    moz_emu.download(david_prefix + 'template.css')
+    moz_emu.download(david_prefix + 'scripts/sidebar.js')
+    moz_emu.download(david_prefix + 'summary.js')
 
-    userDownloadPage = MozEmu.download('http://david.abcc.ncifcrf.gov/chartReport.jsp?annot=,GOTERM_BP_5,GOTERM_CC_5,GOTERM_MF_5,KEGG_PATHWAY&currentList=0')
-    index = userDownloadPage.find('UserDownload')
-    addr = userDownloadPage[index+13:].split('.')[0]
-    res = MozEmu.download('http://david.abcc.ncifcrf.gov/UserDownload/' + addr  + '.txt')
+    user_download_page = moz_emu.download(david_prefix
+                                          + 'chartReport.jsp?annot=,'
+                                          + david_cats + '&currentList=0')
+    index = user_download_page.find('UserDownload')
+    addr = user_download_page[index+13:].split('.')[0]
+    res = moz_emu.download('http://david.abcc.ncifcrf.gov/UserDownload/'
+                           + addr  + '.txt')
     return cat2term2protein_sig(res.split('\n')[1:-1])
