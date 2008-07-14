ecfcebe56ab444b8bfe6de7f28622a616bfabc00
walldo2 <walldo2@35a78be9-df47-0410-acef-f5dffbb544d5>
2008-07-14 22:44:50 +0000

    Code update

--- trunk/will/PyELM/HIVDatabase.py
+++ trunk/will/PyELM/HIVDatabase.py
@@ -1,6 +1,5 @@
 from __future__ import with_statement
 import PyVirus
-import shelve
 import numpy
 from Bio import SeqIO
 import itertools
@@ -9,26 +8,13 @@ import re
 import os
 import threading
 import copy
+import logging
+import cPickle as pickle
+import types
 
 ref_source = os.environ['MYDOCPATH'] + 'hivsnppredsvn\\HIVRefs\\'
 dest_dir = "C:\\local_blast\\PyELMData\\"
 bkg_file = os.environ['MYDOCPATH'] + 'PyELM\\50_seqs.fasta'
-
-
-def MakeBatch(ITERABLE, NUM_PER_BATCH):
-    """
-    MakeBatch
-        Zips an iterable into batches and then yields the batches
-    """
-    internal_list = []
-    for this_iter in ITERABLE:
-        internal_list.append(this_iter)
-        if len(internal_list) == NUM_PER_BATCH:
-            yield copy.deepcopy(internal_list)
-            internal_list = []
-    if len(internal_list) != 0:
-        yield copy.deepcopy(internal_list)
-
 
 class MappingRecord():
     def __init__(self, REF_NAME, TEST_VIRAL):
@@ -65,13 +51,19 @@ class MappingRecord():
 
 
 class MappingBase():
-    def __init__(self, REF_SOURCE, BKG_FILE, DEST_DIR, SHELF_NAME):
+    def __init__(self, REF_SOURCE, DEST_DIR, SHELF_NAME):
         self.ref_source = REF_SOURCE
-        self.bkg_source = BKG_FILE
         self.dest_dir = DEST_DIR
         self.ref_base = PyVirus.RefBase(REF_SOURCE, DEST_DIR)
         self.my_shelf_name = DEST_DIR + SHELF_NAME
-        self.my_shelf = shelve.open(self.my_shelf_name)
+        try:
+            with open(self.my_shelf_name) as prev_handle:
+                self.my_shelf = pickle.load(prev_handle)
+        except IOError:
+            self.my_shelf = {}
+            
+           
+        
 
     def BuildRefBase(self):
         """
@@ -80,51 +72,55 @@ class MappingBase():
         """
         self.ref_base.BuildDatabase()
 
-    def CreateShelf(self, MULTI_THREAD = False):
+    def AddtoShelf(self, INPUT_SEQRECORD_ITER, MULTI_THREAD = False):
         """
-        CreateShelf
+        AddtoShelf
             Blasts the background sequences against the RefBase and creates
-        shelf instances of the resulting alignments.
+        shelf instances of the resulting alignments.  Input must be a LIST,
+        ITER or GENERATOR
         """
-
-        BATCH_SIZE = 1
         
-        with open(self.bkg_source, mode = 'r') as bkg_handle:
-            bkg_iter = SeqIO.parse(bkg_handle, 'fasta')
-        
-            if not(MULTI_THREAD):    
-                for this_seq_batch in MakeBatch(bkg_iter, BATCH_SIZE):
-                    this_blast = self.ref_base.BLASTn(this_seq_batch)
-                    this_seq_batch.reverse()
-                    
-                    for this_mapping in self.MapToRefs(this_blast,
-                                                       this_seq_batch):
-                        volume_name = this_mapping.ref_name
-                        volume_name += this_mapping.test_viral_seq.seq_name
+           
+        for this_seq in INPUT_SEQRECORD_ITER:
+            this_blast = self.ref_base.BLASTn(this_seq)
 
-                        self.my_shelf[volume_name] = this_mapping
+            for this_mapping in self.MapToRefs(this_blast,
+                                           this_seq):
+                volume_name = this_mapping.ref_name
+                volume_name += this_mapping.test_viral_seq.seq_name
+                print volume_name
 
-                
-                
-                
+                self.my_shelf[volume_name] = this_mapping
+
+    def SaveShelf(self):
+        """
+        Saves the precalculated dictionary
+        """
+        with open(self.my_shelf_name, mode = 'w') as prev_handle:
+            pickle.dump(self.my_shelf, prev_handle)
+
         
+             
+
     def MapToRefs(self, BLAST_RECORDS, SEQ_RECORD):
         """
-        MapToRefs
+            MapToRefs
             Use a sequence of BLAST_RECORDs to create a sequence of
-            MappingRecord.
+        MappingRecord.
         """
         #reg_exp gets the Genbank ID from the header
 
-        for this_rec in itertools.izip(BLAST_RECORDS, iter(SEQ_RECORD)):
-            checker = re.match('.*?\|(\w{1,2}\d*\.\d*).*', this_rec[0].title)
-            ref_name = str(checker.groups()[0])
-            this_bkgseq = PyVirus.BkgSeq(this_rec[1], None)
+        checker = re.compile('.*?\|(\w{1,2}\d*\.\d*).*')
+
+
+        for this_align in BLAST_RECORDS.alignments:
+            temp_name = checker.match(this_align.title)
+            ref_name = str(temp_name.groups()[0])
+            this_bkgseq = PyVirus.BkgSeq(SEQ_RECORD, None)
             this_mapping = MappingRecord(ref_name, this_bkgseq)
-            this_mapping.CalculateMapping(this_rec[0])
+            this_mapping.CalculateMapping(this_align)
 
             yield this_mapping
 
-    
-        
-        
+
+
