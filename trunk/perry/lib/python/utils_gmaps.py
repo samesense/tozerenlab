dc9b95829d580357b9cd3a47b14e4f767c29a3db
samesense <samesense@35a78be9-df47-0410-acef-f5dffbb544d5>
2008-07-18 12:58:05 +0000

    adding+gmaps+utils

---
+++ trunk/perry/lib/python/utils_gmaps.py
@@ -0,0 +1,42 @@
+#-----------------------------------------
+#  Author:     Perry Evans
+#              evansjp@mail.med.upenn.edu
+#  2008
+#
+#-----------------------------------------
+"""
+Functions interacting with google maps.
+"""
+import PyMozilla, re
+
+def formatAddr(addr):
+    """ Replace space/comma combo with %2C+ and space with +.
+
+    @param addr: use input address
+    @return formatted address for googlemaps query
+    """
+
+    comma_sub = re.sub(",\s", '%2C+', addr)
+    return re.sub(' ', '+', comma_sub)
+
+def drivingDistance(here, there):
+    """ Returns the driving distance
+        from here to there as determined 
+        by google maps.  Works for cities.
+
+    @param here: location driving from
+    @param there: location driving to
+    @return: float distance in mi
+    """
+
+    googlemaps = 'http://maps.google.com/'
+    here = formatAddr(here)
+    there = formatAddr(there)
+    moz_emu = PyMozilla.MozillaEmulator(cacher=None, trycount=0)
+    web_page = moz_emu.download(googlemaps + 
+                                + 'maps?f=d&source=s_d&hl=en&geocode=&saddr='
+                                + here
+                                + '&daddr='
+                                + there
+                                + '&btnG=Get+Directions&output=js')
+    return web_page.split('timedist')[1].split('mi')[0].split('\\')[-2].split('e')[1].replace(',' ,'')
