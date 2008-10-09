46e3dc83e634ca8ee3e13c8dd50f2b3153eed392
walldo2 <walldo2@35a78be9-df47-0410-acef-f5dffbb544d5>
2008-10-09 21:04:04 +0000

    Added a CheckList method to PyHyperGeo.py which can take two set()'s and a background and determine the p-value of the overlap ... added @memoize to speed up processing dractically ... added tests to testHyperGeo.py to confirm the new functionality

--- will/PyUtils/PyHyperGeo.py
+++ will/PyUtils/PyHyperGeo.py
@@ -6,9 +6,85 @@ PyHyperGeo
 """
 from decimal import *
 import itertools as IT
+import types
+
+
+class memoized(object):
+   """Decorator that caches a function's return value each time it is called.
+   If called later with the same arguments, the cached value is returned, and
+   not re-evaluated.
+   """
+   def __init__(self, func):
+      self.func = func
+      self.cache = {}
+   def __call__(self, *args):
+      try:
+         return self.cache[args]
+      except KeyError:
+         self.cache[args] = value = self.func(*args)
+         return value
+      except TypeError:
+         # uncachable -- for instance, passing a list as an argument.
+         # Better to not cache than to blow up entirely.
+         return self.func(*args)
+   def __repr__(self):
+      """Return the function's docstring."""
+      return self.func.__doc__
 
 
 
+def CheckList(SET_1, SET_2, BACKGROUND):
+	"""
+	Determines the p-value of list overlaps based on the HyperGeometric 
+	distribution.
+	
+	@param:	SET_1			A set() of the elements which were 'picked' by an
+							analysis method.
+	
+	@param:	SET_2			A set() of the elements which were 'picked' by a 
+							seperate analysis method.
+	
+	@param:	BACKGROUND		A set() of the elements which COULD HAVE been
+							picked by BOTH methods.
+	
+	@returns:				The p-value of the overlap based on the 
+							hypergeometric distribution.
+	
+	Example:
+		BACKGROUND:			All genes which have upstream promoters
+		SET_1:				All genes which have the FOXD3 TF binding site
+		SET_2:				All genes found by a SAM microarray analysis.
+	
+		returns:			The p-value of finding the overlap between the SAM
+							analysis and the FOXD3 list.
+	"""
+	
+	if type(SET_1) == types.ListType:
+		raise TypeError, 'PICKED must be a set()'
+	if type(SET_2) == types.ListType:
+		raise TypeError, 'SET_2 must be a set()'
+	if type(BACKGROUND) == types.ListType:
+		raise TypeError, 'BACKGROUND must be a set()'
+	
+	if not(SET_1.issubset(BACKGROUND)):
+		raise ValueError, 'SET_1 must be a subset of BACKGROUND'
+	if not(SET_2.issubset(BACKGROUND)):
+		raise ValueError, 'SET_2 must be a subset of BACKGROUND'
+	
+	N = len(BACKGROUND)
+	k = len(SET_1 & SET_2)
+	m = len(SET_2)
+	n = len(SET_1)
+	
+	print (k - 1, N, m, n)
+	
+	return Decimal(1) - HyperGeoCDF(k - 1, N, m, n)
+	
+	
+	
+
+
+@memoized
 def nchoosek(n, k):
 	"""
 	returns the number of possible ways of choosing k items from a group of a 
@@ -55,6 +131,7 @@ def HyperGeoCDF(k, N, m, n):
 		raise ValueError, 'N:%(N)s must be larger than m:%(m)s' %\
 							{'N':N, 'm':m}
 	
+	
 	prob = 0
 	const_denom = nchoosek(N,n)
 	n_ch_k_dict = {}
@@ -68,7 +145,8 @@ def HyperGeoCDF(k, N, m, n):
 		prob += n_ch_k_dict[(m,i)]*n_ch_k_dict[(N-m, n-i)]/const_denom
 	
 	return prob
-	
+
+@memoized
 def HyperGeoPDF(k, N, m, n):
 	"""
 	There is a shipment of N objects in which m are defective. HyperGeoCDF 
